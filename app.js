fd52acd66289ef3170ddfa8749c9b46aa814e5a2
YuliaBardakova <108867938+YuliaBardakova@users.noreply.github.com>
2023-02-07 14:40:08 +0200

    hj

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const Joi = require("joi");
+// const Joi = require("joi");
+function add(a, b, c) {
 
-function add(a,b,c) {
-    console.log(`Addition result equals ${a + b + c}`);
-  }
-  add(15, 27, 10);
-  add(10, 20, 30);
-  add(5, 10, 15);
+return(a + b + c);
+}
+
+add(2, 5, 8); // 15
+
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
+
